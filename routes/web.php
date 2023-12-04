2c51d61c9a9aab7c8b6e94ee067fc846f6513646
Kareem Mohamed <kotbekareem74@gmail.com>
2023-12-04 16:31:31 -0500

    edit routes

--- routes/web.php
+++ routes/web.php
@@ -13,22 +13,22 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-Route::get('/', function () {
+Route::get('/sys', function () {
     return view('welcome');
 });
-Route::get('/add-reprsentative', function () {
+Route::get('/sys/add-reprsentative', function () {
     return view('add_reprsentative');
 })->name('add.reprsentative');
-Route::post('/add-reprsentative', [ReprsentativeController::class, "put"])->name('reprsentative.put');
-Route::get('/reprsentatives', [ReprsentativeController::class, "index"])->name('reprsentative.prev');
-Route::get('/reprsentative/{id}', [ReprsentativeController::class, "prev"])->name('reprsentative.show');
-Route::get('/edit-reprsentative/{id}', [ReprsentativeController::class, "edit"])->name('reprsentative.edit');
-Route::post('/update-reprsentative', [ReprsentativeController::class, "update"])->name('reprsentative.update');
-Route::post('/get-reprsentative', [ReprsentativeController::class, "getReprsentatives"])->name('reprsentatives.get');
-Route::post('/search-reprsentative', [ReprsentativeController::class, "search"])->name('reprsentatives.search');
-Route::post('/delete-reprsentative', [ReprsentativeController::class, "delete"])->name('reprsentatives.delete');
+Route::post('/sys/add-reprsentative', [ReprsentativeController::class, "put"])->name('reprsentative.put');
+Route::get('/sys/reprsentatives', [ReprsentativeController::class, "index"])->name('reprsentative.prev');
+Route::get('/sys/reprsentative/{id}', [ReprsentativeController::class, "prev"])->name('reprsentative.show');
+Route::get('/sys/edit-reprsentative/{id}', [ReprsentativeController::class, "edit"])->name('reprsentative.edit');
+Route::post('/sys/update-reprsentative', [ReprsentativeController::class, "update"])->name('reprsentative.update');
+Route::post('/sys/get-reprsentative', [ReprsentativeController::class, "getReprsentatives"])->name('reprsentatives.get');
+Route::post('/sys/search-reprsentative', [ReprsentativeController::class, "search"])->name('reprsentatives.search');
+Route::post('/sys/delete-reprsentative', [ReprsentativeController::class, "delete"])->name('reprsentatives.delete');
 
-Route::get('/add-cheque/{id}', [ChequeController::class, "add"])->name('cheque.add');
-Route::post('/add-cheque', [ChequeController::class, "put"])->name('cheque.put');
-Route::get('/Cheque/{id}', [ChequeController::class, "prev"])->name('cheque.prev');
-Route::post('/delete-cheque', [ChequeController::class, "delete"])->name('cheque.delete');
+Route::get('/sys/add-cheque/{id}', [ChequeController::class, "add"])->name('cheque.add');
+Route::post('/sys/add-cheque', [ChequeController::class, "put"])->name('cheque.put');
+Route::get('/sys/Cheque/{id}', [ChequeController::class, "prev"])->name('cheque.prev');
+Route::post('/sys/delete-cheque', [ChequeController::class, "delete"])->name('cheque.delete');
